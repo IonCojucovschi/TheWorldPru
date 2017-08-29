99ff7804bb2dbaf0956a9862f23828caf4a0517f
IonCojucovschi <ion.cojucovschi@outlook.com>
2017-08-29 19:00:02 +0300

    Work Comit

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,52 @@
+﻿function aply() {
+ 
+    //var ele = $("#username");///$  jquery    # in string 
+    //ele.text("Shawn Wildermutl");
+
+
+    //var main =$("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background:#888";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide sidebar");
+        }
+
+
+    });
+    
+
+
+}
+aply();
+
+
+
+
+
+
+
+
+
+
+
+
